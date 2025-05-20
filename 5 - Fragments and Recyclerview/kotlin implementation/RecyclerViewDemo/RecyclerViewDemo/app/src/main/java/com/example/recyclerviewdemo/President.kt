93514270b1d820008b4b3c540270937a6ffa82a3
robitussin <elizerponiojr@gmail.com>
2025-05-20 08:33:07 +0800

    removed extra folder

--- 5 - Fragments and Recyclerview/kotlin implementation/RecyclerViewDemo/RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/President.kt
+++
@@ -1,9 +0,0 @@
-package com.example.recyclerviewdemo
-
-import androidx.annotation.DrawableRes
-
-data class President(
-    val name: String,
-    val order: String,
-    @DrawableRes val portrait: Int
-)
--- 5 - Fragments and Recyclerview/kotlin implementation/RecyclerViewDemo/RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/PresidentId.kt
+++
@@ -1,5 +0,0 @@
-package com.example.recyclerviewdemo
-
-enum class PresidentId {
-            FORD, CARTER, REAGAN, BUSH1, CLINTON, BUSH2, OBAMA, TRUMP1, BIDEN, TRUMP2
-}
